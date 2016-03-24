94db63d48b02ca44dc4c79d4b62e7146aa28ae3b
cpk26 <cpk26@cam.ac.uk>
2016-03-24 17:55:03 +0000

    Moved over to fminunc() for optimization routine

--- ism_inv_optfunc.m
+++ ism_inv_optfunc.m
@@ -1,5 +1,5 @@
 
-function [cst,grad] = ism_invoptWrapper(acoeff,vv,aa, pp, gg, oo)
+function [cst,grad] = ism_inv_optfunc(acoeff,vv,aa, pp, gg, oo)
 % Inputs:
 %   vv      struct containing initial solution variables
 %   aa      prescribed fields, including inputs and boundary conditions
@@ -9,15 +9,19 @@ function [cst,grad] = ism_invoptWrapper(acoeff,vv,aa, pp, gg, oo)
 % Outputs:
 %   vv2     updated struct with new alpha coefficients
 
-vv.acoeff = acoeff;
+vv.acoeff = reshape(acoeff,gg.nJ,gg.nI);   %Array=>matrix
+vv.C = ism_cslip_field(vv, pp, gg, oo);    %reconstruct basal slipperiness
 
 [vv] = ism_sia(aa.s,aa.h,vv.C,vv, pp,gg,oo);   %SIA                                        
 [vv] = ism_sstream(vv,aa,pp,gg,oo );          %SSA 
+[vv] = ism_adjoint(vv,aa,pp,gg,oo );
 [vv] = ism_cost_jac(vv, pp, gg, oo);
 
-grad = vv.cJac;                      %Gradient
 cst = ism_inv_cost(vv,aa,pp,gg, oo); %Current misfit
 
+if nargout > 1 % gradient required
+    grad = vv.cJac(:);                      %Gradient (as array)
+end
 
 end   
 
